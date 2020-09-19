a79c0ae953e9f0b03ea26287cf6acaeff2174921
guilleripa <guilleripa21@gmail.com>
2020-09-19 20:51:29 -0300

    Add capacities validator.

--- toolbox.py
+++ toolbox.py
@@ -1,17 +1,58 @@
+import logging
 import random
 
 import numpy as np
 
+logger = logging.getLogger("Toolbox")
 
-def initIterateAndDistribute(container, store_count, vehicle_count):
-    vehicle_routes = random.sample(range(store_count), store_count)
+
+def valid_route_capacity(route, vehicle_idx, instances):
+    """
+    Given a route and a vehicle we check that the vehicle can fulfill the route's demand.
+    """
+    # we are assuming
+    # instances ~= {
+    # 'vehicles': [[driver_rate, vehicle_capacity]],
+    # 'stores': [[..., store_demand]]
+    # }
+    max_capacity = instances["vehicles"][vehicle_idx][-1]
+    current_capacity = 0
+    for store in route:
+        current_capacity += instances["stores"][store][-1]
+        if current_capacity > max_capacity:
+            return False
+    return True
+
+
+def validate_capacities(genome, store_count, instances):
+    # TODO: si vemos que falla en alguna implementamos esto:
+    # Queremos recorrer el genoma e ir llevando la carga total de cada vehículo.
+    # Si vemos que alcanza, está todo bien y seguimos, sino, cortamos ahí y le pasamos las tiendas restantes al siguiente camión.
+    # Si el último camión tiene tiendas sobrantes, entonces movemos la parte 1 para darle esas al primer camión y volvermos a arrancar.
+
+    routes, route_counts = genome[:store_count], genome[store_count:]
+    start = 0
+    for vehicle_idx, route_count in enumerate(route_counts):
+        # we are assuming instances ~= {'vehicles': [[driver_rate, vehicle_capacity]]}
+        route = routes[start : start + route_count]
+        if not valid_route_capacity(route, vehicle_idx, instances):
+            return False
+        start += route_count
+
+    route = routes[start:]
+    return valid_route_capacity(route, len(route_counts), instances)
+
+
+def initIterateAndDistribute(container, store_count, vehicle_count, instances):
+    routes = random.sample(range(store_count), store_count)
 
     # The store_count will be the total sum to keep, then we distribute it between vehicles.
     # Finally we remove the last one to keep solution consistency.
-    vehicle_assignment_counts = np.random.multinomial(
+    route_counts = np.random.multinomial(
         store_count, np.ones(vehicle_count) / vehicle_count, size=1
     )[0][:-1]
 
-    vehicle_routes.extend(vehicle_assignment_counts)
+    routes.extend(route_counts)
 
-    return container(vehicle_routes)
+    assert validate_capacities(routes, store_count, instances)
+    return container(routes)
